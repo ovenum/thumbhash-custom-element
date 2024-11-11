7d3b3ccfb660a71642ac50b4f9e425cadd539915
Rasso Hilber <mail@rassohilber.com>
2024-11-11 16:23:11 +0100

    Update tests

--- tests/e2e/tests/features.spec.ts
+++ tests/e2e/tests/features.spec.ts
@@ -13,7 +13,7 @@ test.describe("Features", () => {
     await page.waitForTimeout(2000);
 
     // Locate the thumb-hash element
-    const canvas = page.getByTestId('demos-1').locator('.demo.--canvas thumb-hash canvas');
+    const canvas = page.getByTestId('demos-1').locator('thumb-hash[strategy="canvas"] canvas');
     expect(canvas).toHaveCount(1);
     expect(canvas).toHaveAttribute('width');
     expect(canvas).toHaveAttribute('height');
@@ -26,8 +26,20 @@ test.describe("Features", () => {
 
     await page.waitForTimeout(2000);
 
-    const div = page.getByTestId('demos-1').locator('.demo.--average thumb-hash div');
+    const div = page.getByTestId('demos-1').locator('thumb-hash[strategy="average"] div');
     expect(div).toHaveCount(1);
     expect(div).toHaveAttribute('style', 'width: 100%; height: 100%; background: rgb(163, 134, 104);');
+  });
+
+  test("renders an image with a data URI", async ({ page }) => {
+    await page.goto('/');
+    page.setViewportSize({ width: 1000, height: 1000 });
+
+    await page.waitForTimeout(2000);
+
+    const div = page.getByTestId('demos-2').locator('thumb-hash[strategy="image"] img');
+    expect(div).toHaveCount(1);
+    expect(div).toHaveAttribute('style', 'width: 100%; height: 100%;');
+    expect(div).toHaveAttribute('alt', '');
   });
 });
